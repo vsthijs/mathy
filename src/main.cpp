67f04f4f251ca83e6844590a3f3978bc82441c05
dMATTb <109466672+dddMATTbbb@users.noreply.github.com>
2023-12-08 21:23:59 +0100

    created "Player" struct

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,46 @@
 #include "raylib.h"
 #include <iostream>
 
-int playerX = 450;
-int playerY = 300;
-unsigned int playerSize = 50;
-int mouseX, mouseY;
+//unsigned int playerSize = 50;
+//int mouseX, mouseY;
+
+struct Player
+{
+    float x, y;
+    int width, height;
+
+    void Draw()
+    {
+        DrawRectangle(x, y, width, height, BLUE);
+    }
+};
 
 int main(int argc, char** argv) {
     Window win{"mathy", 900, 600};
     AssetManager assets{ASSETS_PATH};
+
+    //setting player variables
+    Player player;
+    player.x = 450;
+    player.y = 300;
+    player.width = 60;
+    player.height = 100;
 
     SetTargetFPS(60);
     SetExitKey(KEY_Q);
 
     while (!WindowShouldClose()) {
         //code for the movement player
-        if (IsKeyDown(KEY_W)) playerY -= 2.0f; //going up
-        if (IsKeyDown(KEY_S)) playerY += 2.0f; //going down
-        if (IsKeyDown(KEY_A)) playerX -= 2.0f; //moving left
-        if (IsKeyDown(KEY_D)) playerX += 2.0f; //moving right
+        if (IsKeyDown(KEY_W)) player.y -= 2.0f; //going up
+        if (IsKeyDown(KEY_S)) player.y += 2.0f; //going down
+        if (IsKeyDown(KEY_A)) player.x -= 2.0f; //moving left
+        if (IsKeyDown(KEY_D)) player.x += 2.0f; //moving right
 
 
         BeginDrawing();
             ClearBackground(WHITE);
             DrawFPS(10, 10);
-            DrawRectangle(playerX, playerY, 50, 100, BLUE); //drawing the player
+            player.Draw();
             DrawRectangle(200, 300, 70, 90, RED); //drawing the quest giver
             //DrawTextureEx(assets.require_texture("player_front.png"),
             //              Vector2{(float)playerX, (float)playerY}, 0, playerSize / 10, WHITE);
