e70f1f909747d134aa1891525d6367c0a82a8183
dMATTb <109466672+dddMATTbbb@users.noreply.github.com>
2023-12-08 21:03:31 +0100

    moved to normal movement and added a experimental "wizard"

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "raylib.h"
 #include <iostream>
 
-int playerX = 0;
-int playerY = 0;
+int playerX = 450;
+int playerY = 300;
 unsigned int playerSize = 50;
 int mouseX, mouseY;
 
@@ -16,19 +16,20 @@ int main(int argc, char** argv) {
     SetExitKey(KEY_Q);
 
     while (!WindowShouldClose()) {
-        if (IsKeyDown(KEY_W)) playerSize += 1.0f;
-        if (IsKeyDown(KEY_S)) playerSize -= 1.0f;
-        if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON))
-        {
-            playerX = GetMouseX();
-            playerY = GetMouseY();
-        }
+        //code for the movement player
+        if (IsKeyDown(KEY_W)) playerY -= 2.0f; //going up
+        if (IsKeyDown(KEY_S)) playerY += 2.0f; //going down
+        if (IsKeyDown(KEY_A)) playerX -= 2.0f; //moving left
+        if (IsKeyDown(KEY_D)) playerX += 2.0f; //moving right
+
 
         BeginDrawing();
             ClearBackground(WHITE);
             DrawFPS(10, 10);
-            DrawTextureEx(assets.require_texture("player_front.png"),
-                          Vector2{(float)playerX, (float)playerY}, 0, playerSize / 10, WHITE);
+            DrawRectangle(playerX, playerY, 50, 100, BLUE); //drawing the player
+            DrawRectangle(200, 300, 70, 90, RED); //drawing the quest giver
+            //DrawTextureEx(assets.require_texture("player_front.png"),
+            //              Vector2{(float)playerX, (float)playerY}, 0, playerSize / 10, WHITE);
         EndDrawing();
     }
 
