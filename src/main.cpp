f20171064055c5fca000fb2ad7e4bc372e319f6e
Thijs van Straaten <thijsvstraaten@gmail.com>
2023-12-08 11:26:10 +0100

    loaded the first asset

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,28 @@
+#include "assets.hpp"
 #include "window.hpp"
 #include "raylib.h"
 #include <iostream>
 
 const int playerX = 450;
 const int playerY = 300;
-int playerSize = 50;
+unsigned int playerSize = 50;
 
-int main() {
+int main(int argc, char** argv) {
     Window win{"mathy", 900, 600};
+    AssetManager assets{ASSETS_PATH};
+
     // SetTargetFPS(60);
     SetExitKey(KEY_Q);
 
     while (!WindowShouldClose()) {
-        if (IsKeyDown(KEY_W)) playerSize += 1.0f;
-        if (IsKeyDown(KEY_S)) playerSize -= 1.0f;
+        if (IsKeyDown(KEY_W)) playerSize += 1;
+        if (IsKeyDown(KEY_S) && playerSize > 0) playerSize -= 1;
 
         BeginDrawing();
             ClearBackground(WHITE);
             DrawFPS(10, 10);
-            DrawCircle(playerX, playerY, playerSize, RED);
+            DrawTextureEx(assets.require_texture("0.png"),
+                          Vector2{playerX, playerY}, 0, playerSize / 10, WHITE);
         EndDrawing();
     }
 
