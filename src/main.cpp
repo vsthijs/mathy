e0e972add47587708719a4de7c799544a160c946
dMATTb <109466672+dddMATTbbb@users.noreply.github.com>
2023-12-08 10:54:08 +0100

    Experimentend with input and drawing

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,23 @@
 #include "raylib.h"
 #include <iostream>
 
+const int playerX = 450;
+const int playerY = 300;
+int playerSize = 50;
+
 int main() {
     Window win{"mathy", 900, 600};
     // SetTargetFPS(60);
     SetExitKey(KEY_Q);
 
     while (!WindowShouldClose()) {
+        if (IsKeyDown(KEY_W)) playerSize += 1.0f;
+        if (IsKeyDown(KEY_S)) playerSize -= 1.0f;
+
         BeginDrawing();
-        ClearBackground(WHITE);
-        DrawFPS(10, 10);
+            ClearBackground(WHITE);
+            DrawFPS(10, 10);
+            DrawCircle(playerX, playerY, playerSize, RED);
         EndDrawing();
     }
 
