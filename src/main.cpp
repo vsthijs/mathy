c510e7a4098e4d600f98223c34cf349cac9dd761
dMATTb <109466672+dddMATTbbb@users.noreply.github.com>
2023-12-13 08:00:10 +0100

    added deltatime to the movement and made so you cant move diagonal

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,11 @@ struct Player
 {
     float x, y;
     int width, height;
-
-    Rectangle player = {x, y, (float)width, (float)height};
+    float speed;
 
     void Draw()
     {
-        DrawRectangleRec(player, BLUE);
+        DrawRectangle(x, y, width, height, BLUE);
     }
 };
 
@@ -47,6 +46,7 @@ int main(int argc, char** argv) {
     player.y = 300;
     player.width = 60;
     player.height = 100;
+    player.speed = 200.0f;
 
     // setting Wizard variables
     Wizard wizard;
@@ -55,19 +55,30 @@ int main(int argc, char** argv) {
     wizard.width = 70;
     wizard.height = 90;
 
-    SetTargetFPS(60);
+    //SetTargetFPS(60);
     SetExitKey(KEY_Q);
-
-    // TODO: gebruik dit om te bewegen, net als met Unity's deltaTime.
-    float delta = GetFrameTime();
 
     // game loop
     while (!WindowShouldClose()) {
-        // code for the movement player
-        if (IsKeyDown(KEY_W)) player.y -= 2.0f; // going up
-        if (IsKeyDown(KEY_S)) player.y += 2.0f; // going down
-        if (IsKeyDown(KEY_A)) player.x -= 2.0f; // moving left
-        if (IsKeyDown(KEY_D)) player.x += 2.0f; // moving right
+        // TODO: gebruik dit om te bewegen, net als met Unity's deltaTime.
+        float deltatime = GetFrameTime();
+
+        if (IsKeyDown(KEY_W))
+        {
+            player.y -= player.speed * deltatime; // moving up
+        } 
+        else if (IsKeyDown(KEY_S))
+        {
+            player.y += player.speed * deltatime; // moving down
+        } 
+        else if (IsKeyDown(KEY_A))
+        {
+            player.x -= player.speed * deltatime; // moving left
+        }
+        else if (IsKeyDown(KEY_D))
+        {
+            player.x += player.speed * deltatime; // moving right
+        }
 
         if (IsKeyPressed(KEY_SPACE))
         {
